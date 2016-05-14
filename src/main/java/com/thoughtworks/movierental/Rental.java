a480166fe75f79f72afb77ce43c0dafa32552ac9
Nikitha Iyer <nikita9.iyer@gmail.com>
2016-05-14 09:28:28 +0530

    EOD 1

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -1,19 +1,51 @@
 package com.thoughtworks.movierental;
 
 class Rental {
-  private Movie _movie;
-  private int _daysRented;
+  private Movie movie;
+  private int daysRented;
 
   public Rental(Movie movie, int daysRented) {
-    _movie = movie;
-    _daysRented = daysRented;
+    this.movie = movie;
+    this.daysRented = daysRented;
   }
 
   public int getDaysRented() {
-    return _daysRented;
+    return daysRented;
   }
 
   public Movie getMovie() {
-    return _movie;
+    return movie;
+  }
+
+  public double amount() {
+    double amount = 0;
+    switch (movie.getPriceCode()) {
+      case Movie.REGULAR:
+        amount += 2;
+        if (daysRented > 2)
+          amount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        amount += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        amount += 1.5;
+        if (daysRented > 3)
+          amount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return amount;
+  }
+
+  public int frequentRenterPoints() {
+    return isEligibleForBonus() ? 2 : 1;
+  }
+
+  private boolean isEligibleForBonus() {
+    return isNewRelease() && daysRented > 1;
+  }
+
+  private boolean isNewRelease() {
+    return movie.getPriceCode() == Movie.NEW_RELEASE;
   }
 }
--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,14 +1,64 @@
 package com.thoughtworks.movierental;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CustomerTest {
 
-    @Test
-    public void testJunit() {
-        assertEquals(true, true);
+    private Customer customer;
+
+    @Before
+    public void setup() {
+        customer = new Customer("Chirag");
+        Movie jungleBook = new Movie("Jungle Book", Movie.CHILDRENS);
+        Movie inception = new Movie("Inception", Movie.REGULAR);
+        Movie civilWar = new Movie("Civil War", Movie.NEW_RELEASE);
+        customer.addRental(new Rental(jungleBook, 2));
+        customer.addRental(new Rental(inception, 6));
+        customer.addRental(new Rental(civilWar, 3));
     }
+
+    @Test
+    public void textStatement() {
+
+        assertEquals("Rental Record for Chirag\n" +
+            "\tJungle Book\t1.5\n" +
+            "\tInception\t8.0\n" +
+            "\tCivil War\t9.0\n" +
+            "Amount owed is 18.5\n" +
+            "You earned 4 frequent renter points", customer.statement());
+    }
+
+//    @Test
+//    @Ignore
+//    public void hTMLStatement() {
+//
+//        assertEquals("<h1>Rental Record for <b>Chirag</b></h1></br>\n" +
+//        "Jungle Book 1.5</br>\n" +
+//            "Inception 8.0</br>\n" +
+//            "Civil War 9.0</br>\n" +
+//            "Amount owed is <b>18.5</b></br>\n" +
+//            "You earned <b>4</b> frequent renter points", customer.htmlStatement());
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void hTMLStatementForFourMovies() {
+//        customer.addRental(new Rental(new Movie("ABCD2", Movie.REGULAR),1));
+//
+//        assertEquals("<h1>Rental Record for <b>Chirag</b></h1></br>\n" +
+//        "Jungle Book 1.5</br>\n" +
+//            "Inception 8.0</br>\n" +
+//            "Civil War 9.0</br>\n" +
+//            "ABCD2 2.0</br>\n" +
+//            "Amount owed is <b>20.5</b></br>\n" +
+//            "You earned <b>5</b> frequent renter points", customer.htmlStatement());
+//    }
+
+
+
 
 }
