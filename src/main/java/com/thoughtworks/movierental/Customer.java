0358015a5f473fa68ebe7195a315107bafe2ed8d
arathyjan <arathyjanardhanan@gmail.com>
2016-07-28 08:35:26 +0530

    Revert "EOD 1"
    This reverts commit a480166fe75f79f72afb77ce43c0dafa32552ac9.

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -1,46 +1,66 @@
 package com.thoughtworks.movierental;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Enumeration;
+import java.util.Vector;
 
 class Customer {
-  private String name;
-  private List<Rental> rentals = new ArrayList<>();
+  private String _name;
+  private Vector _rentals = new Vector();
 
   public Customer(String name) {
-    this.name = name;
+    _name = name;
   }
 
   public void addRental(Rental arg) {
-    rentals.add(arg);
+    _rentals.addElement(arg);
   }
 
   public String getName() {
-    return name;
+    return _name;
   }
 
   public String statement() {
-    return new TextStatement().print(name, rentals, totalAmount(), totalPoints());
-  }
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    Enumeration rentals = _rentals.elements();
+    String result = "Rental Record for " + getName() + "\n";
+    while (rentals.hasMoreElements()) {
+      double thisAmount = 0;
+      Rental each = (Rental) rentals.nextElement();
+      //determine amounts for each line
+      switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+            thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+            thisAmount += (each.getDaysRented() - 3) * 1.5;
+          break;
+      }
+      // add frequent renter points
+      frequentRenterPoints++;
+      // add bonus for a two day new release rental
+      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+          &&
+          each.getDaysRented() > 1) frequentRenterPoints++;
 
-  private int totalPoints() {
-    int total = 0;
-    for (Rental rental : rentals) {
-      total += rental.frequentRenterPoints();
+      //show figures for this rental
+      result += "\t" + each.getMovie().getTitle() + "\t" +
+          String.valueOf(thisAmount) + "\n";
+      totalAmount += thisAmount;
     }
-    return total;
+    //add footer lines result
+    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints)
+        + " frequent renter points";
+    return result;
   }
 
-  private double totalAmount() {
-    double total = 0;
-    for (Rental rental : rentals) {
-      total += rental.amount();
-    }
-    return total;
-  }
-
-  public String htmlStatement() {
-    return new HTMLStatement().print(name, rentals, totalAmount(), totalPoints());
-  }
 }
 
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -1,51 +1,19 @@
 package com.thoughtworks.movierental;
 
 class Rental {
-  private Movie movie;
-  private int daysRented;
+  private Movie _movie;
+  private int _daysRented;
 
   public Rental(Movie movie, int daysRented) {
-    this.movie = movie;
-    this.daysRented = daysRented;
+    _movie = movie;
+    _daysRented = daysRented;
   }
 
   public int getDaysRented() {
-    return daysRented;
+    return _daysRented;
   }
 
   public Movie getMovie() {
-    return movie;
-  }
-
-  public double amount() {
-    double amount = 0;
-    switch (movie.getPriceCode()) {
-      case Movie.REGULAR:
-        amount += 2;
-        if (daysRented > 2)
-          amount += (daysRented - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        amount += daysRented * 3;
-        break;
-      case Movie.CHILDRENS:
-        amount += 1.5;
-        if (daysRented > 3)
-          amount += (daysRented - 3) * 1.5;
-        break;
-    }
-    return amount;
-  }
-
-  public int frequentRenterPoints() {
-    return isEligibleForBonus() ? 2 : 1;
-  }
-
-  private boolean isEligibleForBonus() {
-    return isNewRelease() && daysRented > 1;
-  }
-
-  private boolean isNewRelease() {
-    return movie.getPriceCode() == Movie.NEW_RELEASE;
+    return _movie;
   }
 }
